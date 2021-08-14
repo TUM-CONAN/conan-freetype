0eac4df1e0ea0a60f6051ce22c7cc6322192a39f
Ulrich Eck <ulrich.eck@tum.de>
2021-08-14 08:06:16 +0200

    moved to camp_common

--- conanfile.py
+++ conanfile.py
@@ -7,8 +7,12 @@ from conans import ConanFile, tools, CMake
 
 
 class LibFreetypeConan(ConanFile):
+    python_requires = "camp_common/[>=0.1]@camposs/stable"
+    python_requires_extend = "camp_common.CampCMakeBase"
+
+
     name = "freetype"
-    package_revision = "-r5"
+    package_revision = "-r6"
     upstream_version = "2.9.1"
     version = "{0}{1}".format(upstream_version, package_revision)
     description = ("FreeType is a library used to render text onto bitmaps,"
@@ -25,24 +29,22 @@ class LibFreetypeConan(ConanFile):
     ]
     url = "https://git.ircad.fr/conan/conan-freetype"
     source_subfolder = "source_subfolder"
-    build_subfolder = "build_subfolder"
 
     def requirements(self):
-        self.requires("ircad_common/1.0.3@camposs/stable")
         if tools.os_info.is_windows:
-            self.requires("zlib/1.2.11@camposs/stable")
+            self.requires("zlib/1.2.11-r1@camposs/stable")
 
     def source(self):
         freetype_upstream_url = "https://download.savannah.gnu.org/releases/freetype/freetype-{0}.tar.bz2"
         tools.get(freetype_upstream_url.format(self.upstream_version))
         os.rename("freetype-" + self.upstream_version, self.source_subfolder)
 
-    def build(self):
+    def _before_configure(self):
         freetype_source_dir = os.path.join(self.source_folder, self.source_subfolder)
         tools.patch(freetype_source_dir, "patches/CMakeLists.patch")
 
         # Import common flags and defines
-        import common
+        common = self.python_requires["camp_common"].module
 
         # Generate Cmake wrapper
         common.generate_cmake_wrapper(
@@ -51,15 +53,8 @@ class LibFreetypeConan(ConanFile):
             build_type=self.settings.build_type
         )
 
-        cmake = CMake(self)
-
+    def _before_build(self, cmake):
         cmake.definitions["DISABLE_FORCE_DEBUG_POSTFIX"] = "ON"
         if not tools.os_info.is_windows:
             cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = "ON"
 
-        cmake.configure(source_folder=self.source_subfolder)
-        cmake.build()
-        cmake.install()
-
-    def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
